35482e3a2e60f259de88144077051e5c389ab490
kazirimon1 <kazirimon33@gmail.com>
2023-09-24 00:08:34 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <h1>This is biggest Home Page</h1>
       <h2>My Name is Rimon</h2>
-      <h3>This is the react git hub hosting Live site</h3>
+      <h3>This is the hosting Live site</h3>
     </div>
   );
 }
