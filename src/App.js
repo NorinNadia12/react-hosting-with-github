56f049e7e408c49cea854d47188f227f5dd938b0
kazirimon1 <kazirimon33@gmail.com>
2023-09-25 20:06:08 +0600

    react live site commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
       <h1>This is biggest Home Page</h1>
       <h2>My Name is Rimon</h2>
       <h3>This is the hosting Live site</h3>
+      <h4>This is the live site React Hosting</h4>
+
     </div>
   );
 }
